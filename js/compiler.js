d8bbe082f326148aad277746e863deada9a8d1e2
felicity <yun.gao@mayihr.com>
2020-08-04 11:22:42 +0800

    重写compiler 函数

--- js/compiler.js
+++ js/compiler.js
@@ -1,27 +1,36 @@
 class Compiler {
   constructor(el, vm) {
-    this.$el = document.querySelector(el);
-    this.$vm = vm
-    this.$fragment = this.node2Fragment(this.$el)
-    this.compiler(this.$fragment)
-    this.$el.appendChild(this.$fragment)
+    this.el = this.isElementNode(el) ? el : document.querySelector(el);
+    this.vm = vm;
+    // 把节点保存为文档碎片
+    this.fragment = this.node2fragment(this.el)
+    // 编译文档
+    this.compiler(this.fragment)
+    // 把编译好的节点放回到根节点
+    this.el.appendChild(this.fragment)
   }
-  node2Fragment(el) {
-    let fragment = document.createDocumentFragment()
-    let child
-    while (child = el.firstChild) {
-      fragment.appendChild(child)
+  isElementNode(el) {
+    return el.nodeType === 1
+  }
+  node2fragment(el) {
+    let f = document.createDocumentFragment();
+    let childNodes;
+    while (childNodes = el.firstChild) {
+      f.appendChild(childNodes)
     }
-    return fragment
+    return f
   }
-  compiler(node) {
-    Array.from(node.childNodes).forEach(node => {
-      if (node.nodeType == 1) {
-      } else if (this.isInter(node)) {
-        this.compilerText(node)
+  compiler(el) {
+    let children = el.childNodes;
+    [...children].forEach(child => {
+      if (child.nodeType == 1) {  // 元素节点
+        this.compilerElement(child)
+      } else if (this.isInter(child)) {
+        this.compilerText(child)
       }
-      if (node.children && node.childNodes.length > 0) {
-        this.compiler(node)
+      // 递归循环子节点
+      if (child.children && child.childNodes.length > 0) {
+        this.compiler(child)
       }
     })
   }
@@ -29,11 +38,66 @@ class Compiler {
     return node.nodeType == 3 && /\{\{(.*)\}\}/g.test(node.textContent)
   }
   compilerText(node) {
-    node.nodeValue = this.getValue(RegExp.$1)
+    compilerUtil.text(node, RegExp.$1, this.vm)
   }
-  getValue(val) {
-    return val.split('.').reduce((a, c) => {
-      return a[c]
-    }, this.$vm.$data)
+  compilerElement(node) {
+    let attributes = node.attributes;
+    [...attributes].forEach((attr) => {
+      let { name, value } = attr
+      if (this.isDirective(name)) {
+        let [, directive] = name.split('-');
+        let [dirName, eventName] = directive.split(':');
+        compilerUtil[dirName](node, value, this.vm, eventName)
+        // 操作完成后删除属性
+        node.removeAttribute(name)
+      } else if (this.isSimpleEvent(name)) {
+        let eventName = name.substring(1)
+        compilerUtil.on(node, value, this.vm, eventName)
+        // 操作完成后删除属性
+        node.removeAttribute(name)
+      }
+
+
+    })
+  }
+  isDirective(str) {
+    return str.startsWith('v-');
+  }
+  isSimpleEvent(str) {
+    return str.startsWith('@')
+  }
+}
+const compilerUtil = {
+  getValue(val, vm) {
+    return val.split('.').reduce((data, cur) => {
+      return data[cur]
+    }, vm.$data)
+  },
+  setValue(val, vm, target) {
+    let arr = val.split('.')
+    let len = arr.length - 1
+    arr.reduce((data, cur, currentIndex) => {
+      if (currentIndex == len) {
+        data[cur] = target
+      }
+      return data[cur]
+    }, vm.$data)
+  },
+  text(node, expr, vm) {
+    node.textContent = this.getValue(expr, vm)
+  },
+  html(node, expr, vm) {
+    node.innerHTML = vm.$data[expr]
+  },
+  model(node, expr, vm) {
+    node.value = this.getValue(expr, vm)
+    node.addEventListener('input', (e) => {
+      this.setValue(expr, vm, e.target.value)
+      console.log(vm)
+    })
+  },
+  on(node, expr, vm, eventName) {
+    let fn = vm.$options.methods && vm.$options.methods[expr]
+    node.addEventListener(eventName, fn.bind(vm), false)
   }
 }
